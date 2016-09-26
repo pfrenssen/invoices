be44f286d8821b0c0ad3d37c2a65d755d80d07b1
Pieter Frenssen <pieter@frenssen.be>
2016-09-26 10:50:29 +0200

    Make sure we can select base fields in Business views.

--- web/modules/custom/business/src/Entity/BusinessViewsData.php
+++ web/modules/custom/business/src/Entity/BusinessViewsData.php
@@ -3,12 +3,11 @@
 namespace Drupal\business\Entity;
 
 use Drupal\views\EntityViewsData;
-use Drupal\views\EntityViewsDataInterface;
 
 /**
  * Provides Views data for Business entities.
  */
-class BusinessViewsData extends EntityViewsData implements EntityViewsDataInterface {
+class BusinessViewsData extends EntityViewsData {
 
   /**
    * {@inheritdoc}
@@ -16,11 +15,17 @@ class BusinessViewsData extends EntityViewsData implements EntityViewsDataInterf
   public function getViewsData() {
     $data = parent::getViewsData();
 
-    $data['business']['table']['base'] = array(
+    $data['business']['table']['base'] = [
       'field' => 'id',
       'title' => $this->t('Business'),
       'help' => $this->t('The Business ID.'),
-    );
+    ];
+
+    // Default to the business_field_data table. This will ensure we can select
+    // the base fields without having to manually add a join to this table.
+    $data['business_field_data']['table']['wizard_id'] = 'business';
+    $data['business_field_data']['table']['base']['weight'] = -10;
+    $data['business_field_data']['table']['base']['defaults']['field'] = 'bid';
 
     return $data;
   }
